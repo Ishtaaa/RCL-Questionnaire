6c90699e705b1a6121ba08b4e6874909d0a31b4e
Ishtaaa <Imahadew@outlook.com>
2026-02-09 20:04:14 +0200

    config changes

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: vitePreprocess(),
+  
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false
+    }),
+    
+    // Add this to help with function generation
+    files: {
+      assets: 'static'
+    }
+  }
 };
 
 export default config;
